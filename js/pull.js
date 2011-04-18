1130342b1bfb08e687ef162753fefaa104156ec6
jverkoey <jverkoey@gmail.com>
2011-04-18 09:17:21 -0400

    Add labels to pull requests.

--- js/pull.js
+++ js/pull.js
@@ -1,5 +1,8 @@
 $(function() {
   var prefix = location.pathname.split('/').slice(0, 3).join('/');
+  var issue = location.pathname.split('/').pop();
+  
+  // Build the Issues 2.0 header bar.
   $('.view-pull-request').attr('id', 'issues_next').prepend(
     $('<div>').addClass('browser_header').append(
       $('<ul>').addClass('actions').append(
@@ -74,6 +77,7 @@ $(function() {
     ) // div.browser_header
   );
   
+  // "Back to issue list" link.
   $('.pull-head').addClass('issue-head').prepend(
     $('<p>').addClass('back').append(
       $('<a>').attr({
@@ -83,10 +87,53 @@ $(function() {
     ).attr('style', 'margin-right:20px')
   );
   
+  // Wrap the discussion in the new show_issue div.
+  $('#discussion_bucket').wrap('<div id="show_issue" />');
+  
+  // Issues editor
+  $('.discussion-stats').append(
+    $('<div>').addClass('rule')
+  ).append(
+    $('<div>').addClass('label-manager')/*.append(
+      $('<a>').attr({
+        'class': 'context-button minibutton js-label-context',
+        'href': 'javascript:;'
+      }).append(
+        $('<span>').addClass('icon')
+      ) // a.context-button
+    )*/.append('Labels') // .label-manager
+  ).append(
+    $('<ul>').addClass('labels')
+  );
+  
+  // Enables quicksearch.
   $('#js-issues-quicksearch').quicksearch({
     results: $(this).find(".search .autocomplete-results"),
     insertSpinner: function(f) {
       $("#js-issues-quicksearch").closest("form").prepend(f)
     }
   });
+  
+  // Update this issue's labels
+  $.ajax({
+    dataType: 'json',
+    url: '/api/v2/json/issues/show'+prefix+'/'+issue,
+    failure: function() {
+      // TODO: Show an error on the page.
+    },
+    success: function(data) {
+      var list = $('.discussion-stats .labels');
+      for (var ix in data.issue.labels) {
+        var label = data.issue.labels[ix];
+        list.append(
+          $('<li>').attr({
+            'class': 'label labelstyle-'+label.replace(/[ ]+/, '').toLowerCase()
+          }).append(
+            $('<span>').addClass('name').append(label)
+          )
+        );
+      }
+    }
+  });
+
 });
